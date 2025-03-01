f86878afe11de464fd017c1a81df42da7d5bedea
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-02 02:20:19 +0530

    add product cart widget to home page

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/widgets/app_bar/app_bar.dart';
 import 'package:flutter_app/pages/home_page/widgets/search_bar.dart';
+import 'package:flutter_app/widgets/product_cart/product_cart.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -19,6 +20,19 @@ class HomePage extends StatelessWidget {
               Text(
                 'Explore Categories',
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  ProductCart(
+                    title: "New Item",
+                    decoration: "Hello I'm new item plz buy me!",
+                    titleColor: Colors.black,
+                    descriptionColor: Colors.black87,
+                    productBoxColor: Colors.white,
+                    smallBoxColor: Colors.grey,
+                  ),
+                ],
               ),
             ],
           ),
