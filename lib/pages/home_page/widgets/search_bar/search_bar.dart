4d1edb8a404610ba37e5c5ca2be51f482c5bdbb2
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-02 02:30:00 +0530

    add search bar widget with text input field

--- lib/pages/home_page/widgets/search_bar/search_bar.dart
+++ lib/pages/home_page/widgets/search_bar/search_bar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/home_page/widgets/text_input_field.dart';
+import 'package:flutter_app/pages/home_page/widgets/search_bar/text_input_field.dart';
 
 Widget searchBarBox = Padding(
   padding: EdgeInsets.symmetric(vertical: 15),
