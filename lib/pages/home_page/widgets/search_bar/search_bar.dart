7577a25675a2967652bbc91221d670c7b71b9c69
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-03 02:24:10 +0530

    refactor search bar layout to use column for better alignment and spacing

--- lib/pages/home_page/widgets/search_bar/search_bar.dart
+++ lib/pages/home_page/widgets/search_bar/search_bar.dart
@@ -3,22 +3,27 @@ import 'package:flutter_app/pages/home_page/widgets/search_bar/text_input_field.
 
 Widget searchBarBox = Padding(
   padding: EdgeInsets.symmetric(vertical: 15),
-  child: Container(
-    width: double.infinity,
-    height: 50,
-    decoration: BoxDecoration(
-      color: const Color.fromARGB(115, 199, 199, 199),
-      borderRadius: BorderRadius.circular(50),
-      border: Border.all(color: Colors.black45, width: 1),
-    ),
-    child: Row(
-      children: [
-        Padding(
-          padding: EdgeInsets.fromLTRB(8, 5, 5, 5),
-          child: Icon(Icons.search, color: Colors.black45, size: 35),
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Container(
+        width: double.infinity,
+        height: 50,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(115, 199, 199, 199),
+          borderRadius: BorderRadius.circular(50),
+          border: Border.all(color: Colors.black45, width: 1),
         ),
-        textInputBar,
-      ],
-    ),
+        child: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(8, 5, 5, 5),
+              child: Icon(Icons.search, color: Colors.black45, size: 35),
+            ),
+            textInputBar,
+          ],
+        ),
+      ),
+    ],
   ),
 );
