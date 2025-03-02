2c7189b65bd87b5795ffff0f7cea306d293ba06c
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-03 02:47:41 +0530

    update main.dart to set CategoryPage as the home widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/category_page/category_page.dart';
 import 'package:flutter_app/pages/home_page/home_page.dart';
 
 void main() {
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      //home: HomePage(),
+      home: CategoryPage(),
     );
   }
 }
