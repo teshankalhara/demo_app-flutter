9d41efbaf7fcf1d1b20d6b6c3586de5f8ea9631a
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-03 02:47:33 +0530

    add CategoryPage layout with AppBar and NotificationCard widget

--- lib/pages/category_page/category_page.dart
+++ lib/pages/category_page/category_page.dart
@@ -1,10 +1,34 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/widgets/notification_card/notification_card.dart';
 
 class CategoryPage extends StatelessWidget {
   const CategoryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Categories',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            if (kDebugMode) {
+              print('Back to home page');
+            }
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Column(children: [NotificationCard()]),
+        ),
+      ),
+    );
   }
 }
