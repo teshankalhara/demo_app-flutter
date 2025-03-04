7490fd2bd15a3481249b6847f627aaeec32c2760
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-05 00:58:02 +0530

    update main.dart to set ProdectDetailsPage as the home widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/category_page/category_page.dart';
 import 'package:flutter_app/pages/home_page/home_page.dart';
+import 'package:flutter_app/pages/prodect_details_page/prodect_details_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       //home: HomePage(),
-      home: CategoryPage(),
+      //home: CategoryPage(),
+      home: ProdectDetailsPage(),
     );
   }
 }
