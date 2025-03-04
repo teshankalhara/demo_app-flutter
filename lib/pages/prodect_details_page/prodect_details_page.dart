bab25496b77db92d45c2d5b76dec5a2d5a1436d0
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-03-05 00:03:50 +0530

    add ProdectDetailsPage widget for product detail display

---
+++ lib/pages/prodect_details_page/prodect_details_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProdectDetailsPage extends StatelessWidget {
+  const ProdectDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
